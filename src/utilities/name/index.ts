e3622a2ba9d69d4db44908ba71c4fcf9625934b1
han.kim@domo.com <Han.Kim@domo.com>
2023-08-04 12:00:36 -0600

    Update index.ts

--- src/utilities/name/index.ts
+++ src/utilities/name/index.ts
@@ -1,2 +1,5 @@
 export const getFullName = (firstName: string, lastName: string) =>
   `${firstName} ${lastName}`;
+
+export const getFullNameLength = (firstName: string, lastName: string) =>
+  `${firstName} ${lastName}`.length;
